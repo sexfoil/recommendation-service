2701b4ed7a19ed6305d295b8394fd5fb03247235
Viacheslav Poliakov <viacheslav_poliakov@epam.com>
2022-05-24 14:14:06 +0300

    Initial commit

---
+++ recommendation-service/src/test/java/com/poliakov/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poliakov.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
